51b5d2859f98d557466d59afe6fc758304f1d560
Damian Zsiros Dev <damjeronzalez23@gmail.com>
2022-06-30 14:01:22 -0500

    update main.jsx order of lines

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { store } from "./app/store"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 )
